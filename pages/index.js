53aedf8000cc2560ec73c1d8f9326b7dd5348510
macho-catt <cacho.matthew@gmail.com>
2022-02-25 14:00:28 -0800

    Modify index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
-import { Layout } from '../components';
+import React, { useState, useMemo, createContext } from 'react';
+import dayjs from 'dayjs';
+import { AppHead, Clock } from '../components';
+import layoutStyles from '../styles/pages/layout.styles';
+
+export const ClockContext = createContext();
 
 export default function Home() {
+  const [hour, setHour] = useState(dayjs().format('HH'));
+  const [min, setMin] = useState(dayjs().format('mm'));
+  const [sec, setSec] = useState(dayjs().format('ss'));
+
+  const value = useMemo(() => ({
+      setHour,
+      setMin,
+      setSec,
+    }));
+
   return (
-    <Layout>
-      <div className="container">Hello world</div>
-    </Layout>
+    <div>
+      <AppHead />
+
+      <div id="root" className={layoutStyles.root}>
+        <main className={layoutStyles.main}>
+          <ClockContext.Provider value={value}>
+            <Clock />
+          </ClockContext.Provider>
+
+          <div>
+            {hour}:{min}:{sec}
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
