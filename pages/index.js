4580ff084196119035432f779cac794dceecb85e
macho-catt <cacho.matthew@gmail.com>
2022-02-25 17:48:21 -0800

    Add luminance

--- pages/index.js
+++ pages/index.js
@@ -18,36 +18,58 @@ export default function Home() {
 
   const decToHex = (dec) => {
     let hex = dec.toString(16);
-    hex = hex.length > 1 ? hex : `0${  hex}`;
+    hex = hex.length > 1 ? hex : `0${hex}`;
     return hex.toUpperCase();
   };
 
   const transformToColor = (h, m, s) => {
-    const R = parseInt((h / 24) * 255, 10).toString(16);
-    const G = parseInt((m / 60) * 255, 10).toString(16);
-    const B = parseInt((s / 60) * 255, 10).toString(16);
+    const R = (255 - parseInt((h / 24) * 255, 10)).toString(16);
+    const G = (255 - parseInt((m / 60) * 255, 10)).toString(16);
+    const B = (255 - parseInt((s / 60) * 255, 10)).toString(16);
     const hex = `#${decToHex(R)}${decToHex(G)}${decToHex(B)}`;
     return hex;
   };
 
-  const [color, setColor] = useState(`${transformToColor(hour, min, sec)}`);
-  // const [color, setColor] = useState("bg-[#AAAA15]");
+  const getLuminance = (h, m, s) => {
+    const R = 255 - parseInt((h / 24) * 255, 10);
+    const G = 255 - parseInt((m / 60) * 255, 10);
+    const B = 255 - parseInt((s / 60) * 255, 10);
+    return 0.299 * R + 0.587 * G + 0.114 * B;
+  };
+
+  const [bgColor, setBgColor] = useState(`${transformToColor(hour, min, sec)}`);
+  const [luminance, setLuminance] = useState(getLuminance(hour, min, sec));
+  const [textColor, setTextColor] = useState(
+    luminance > 127.5 ? 'text-black' : 'text-white'
+  );
+
   useEffect(() => {
-    setColor(transformToColor(hour, min, sec));
+    setBgColor(transformToColor(hour, min, sec));
+    setLuminance(getLuminance(hour, min, sec));
   }, [hour, min, sec]);
+
+  useEffect(() => {
+    if (luminance > 127.5) setTextColor('text-black');
+    else setTextColor('text-white');
+  }, [luminance]);
 
   return (
     <div>
       <AppHead />
 
-      <div id="root" style={{ background: `${color}` }}>
+      <div
+        id="root"
+        className={`${textColor}`}
+        style={{ background: `${bgColor}` }}
+      >
         {/* <div id="root" className={`bg-[${color}]`}> */}
         <main className={layoutStyles.main}>
           <ClockContext.Provider value={value}>
             <Clock />
           </ClockContext.Provider>
 
-          <div>{color}</div>
+          <div>{bgColor}</div>
+          <div>{luminance}</div>
         </main>
       </div>
     </div>
