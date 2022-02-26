cbbda7a55542fb95aa43fc4e7b424b0141faa929
macho-catt <cacho.matthew@gmail.com>
2022-02-25 17:20:29 -0800

    Add changing colors

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, createContext } from 'react';
+import React, { useState, useEffect, useMemo, createContext } from 'react';
 import dayjs from 'dayjs';
 import { AppHead, Clock } from '../components';
 import layoutStyles from '../styles/pages/layout.styles';
@@ -11,24 +11,43 @@ export default function Home() {
   const [sec, setSec] = useState(dayjs().format('ss'));
 
   const value = useMemo(() => ({
-      setHour,
-      setMin,
-      setSec,
-    }));
+    setHour,
+    setMin,
+    setSec,
+  }));
+
+  const decToHex = (dec) => {
+    let hex = dec.toString(16);
+    hex = hex.length > 1 ? hex : `0${  hex}`;
+    return hex.toUpperCase();
+  };
+
+  const transformToColor = (h, m, s) => {
+    const R = parseInt((h / 24) * 255, 10).toString(16);
+    const G = parseInt((m / 60) * 255, 10).toString(16);
+    const B = parseInt((s / 60) * 255, 10).toString(16);
+    const hex = `#${decToHex(R)}${decToHex(G)}${decToHex(B)}`;
+    return hex;
+  };
+
+  const [color, setColor] = useState(`${transformToColor(hour, min, sec)}`);
+  // const [color, setColor] = useState("bg-[#AAAA15]");
+  useEffect(() => {
+    setColor(transformToColor(hour, min, sec));
+  }, [hour, min, sec]);
 
   return (
     <div>
       <AppHead />
 
-      <div id="root" className={layoutStyles.root}>
+      <div id="root" style={{ background: `${color}` }}>
+        {/* <div id="root" className={`bg-[${color}]`}> */}
         <main className={layoutStyles.main}>
           <ClockContext.Provider value={value}>
             <Clock />
           </ClockContext.Provider>
 
-          <div>
-            {hour}:{min}:{sec}
-          </div>
+          <div>{color}</div>
         </main>
       </div>
     </div>
