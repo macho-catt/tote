dae5c54ec57fc8a56ad48288a487591d84d5133f
macho-catt <cacho.matthew@gmail.com>
2022-02-27 00:09:45 -0800

    Modify hour value for midnight

--- components/clock.js
+++ components/clock.js
@@ -17,7 +17,7 @@ export default function Clock() {
   };
 
   const toStandard = (h, m, s) => {
-    if (h === 0) return `12:${m}:${s} am`;
+    if (h < 1) return `12:${m}:${s} am`;
     if (h < 12) return `${h}:${m}:${s} am`;
     if (h > 12) return `${h - 12}:${m}:${s} pm`;
     return `${h}:${m}:${s} pm`;
