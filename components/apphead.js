3f4b9cb2a1ed82e5a7a23b0c97ee4bb32ade6762
macho-catt <cacho.matthew@gmail.com>
2022-02-28 13:17:38 -0800

    Add SEO info

--- components/apphead.js
+++ components/apphead.js
@@ -1,10 +1,17 @@
 import Head from 'next/head';
 
 export default function AppHead() {
+  const title = 'tote';
+  const description =
+    'tote (time + quote) displays the local time and a new quote very minute. The background color also changes based on the current time.';
+
   return (
     <Head>
-      <title>Create Next App</title>
+      <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content={description} key="desc" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
     </Head>
   );
 }
