eb0a25dbbc2a9c618288ed43295229d71a052d8a
macho-catt <cacho.matthew@gmail.com>
2022-03-03 16:44:08 -0800

    Add potential code for mocking ssr

--- cypress/integration/quote.spec.js
+++ cypress/integration/quote.spec.js
@@ -7,3 +7,31 @@ describe('Quote', () => {
     cy.get('[data-testid="currQuote"]').should('be.visible');
   });
 });
+
+// const superagent = require("superagent")
+// const mockServer = require("mockttp").getLocal();
+
+// describe('Mockttp', () => {
+//   // Start mock server
+//   beforeEach(async () => {
+//     mockServer.start(8080)
+//     await mockServer.forGet('/mocked-path').thenReply(200, {
+//       q: 'Test quote',
+//       a: 'Test author'
+//     })
+//   })
+
+//   afterEach(async () => {
+//     mockServer.stop()
+//   })
+
+//   it('mocks a request for SSR', () => {
+//     cy.request('GET', 'http://0.0.0.0:3000/', {
+//       method: 'GET',
+//       path: '/mocked-path'
+//     })
+
+//     browser.load('/')
+
+//   })
+// })
